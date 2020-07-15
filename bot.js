5b69cc426323b6b0344e9183dd0f1d20d449fde0
chiwala4dad <chiwala4dad@gmail.com>
2020-07-14 20:49:37 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -37,6 +37,31 @@ client.on('message', message => {
        message.reply('pong');
 
        }
+  else if (message.content === "#pp") {
+
+       // make sure the user is in a voice channel
+       if (message.member.voiceChannel) {
+
+           // join the user's voice channel
+           message.member.voiceChannel.join()
+               .then(connection => {
+
+                   // get a random audio file from our list
+                   const randAudioFile = _.sample(audioFiles);
+
+                   // play the random audio file
+                   const dispatcher = connection.playFile("./data/" + randAudioFile);
+
+                   // disconnect from the voice channel when the quote is over
+                   dispatcher.on("end", () => {
+                       message.member.voiceChannel.leave();
+                       fs.appendFileSync("log.txt", moment().format("YYYY-MM-DD HH:mm:ss.SSS ") + randAudioFile + "\n");
+                   });
+               })
+               .catch(console.log);
+       }
+    
+    }
 
 });
 
