cb25b4f49b161221f43488fbb11b20c87c88ad08
chiwala4dad <chiwala4dad@gmail.com>
2020-07-14 21:15:49 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,17 +9,54 @@ client.on('ready', () => {
     console.log('I am ready!');
 
 });
+client.on("message", message => {
+  function Play(fileName) {
+          message.member.voiceChannel.join()
+               .then(connection => {
+                 // play the random audio file
+                   const dispatcher = connection.playFile("./data/" + fileName );
 
- 
+                   // disconnect from the voice channel when the quote is over
+                   dispatcher.on("end", () => {
+                       message.member.voiceChannel.leave();
+                       
+                   });
+               })
+  
+}
 
-client.on('message', message => {
+
 
     if (message.content === 'ping') {
 
-       message.reply('pong');
+       message.reply('fuck');
 
        }
+   else if (message.content === "cheemz") {
 
+       // make sure the user is in a voice channel
+       if (message.member.voiceChannel) {
+
+           // join the user's voice channel
+           message.member.voiceChannel.join()
+               .then(connection => {
+
+                   // get a random audio file from our list
+                   const randAudioFile = _.sample(audioFiles);
+
+                   // play the random audio file
+                   const dispatcher = connection.playFile("./data/" + randAudioFile);
+
+                   // disconnect from the voice channel when the quote is over
+                   dispatcher.on("end", () => {
+                       message.member.voiceChannel.leave();
+                       
+                   });
+               })
+               
+       }
+    
+    }
 });
 
  
